4a9e951fd7859bdef56adcb7d396d63193942019
Ramon Moreira <ramon.dmm@gmail.com>
2020-09-03 22:18:02 -0300

    First Commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'teste-strapi.wvnih.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'mongo-test'),
+        username: env('DATABASE_USERNAME', 'dbstrapi_teste'),
+        password: env('DATABASE_PASSWORD', 'pcjJjFINq0OHZiTB'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '73e5a8ac-d43f-454f-a870-bef292c0cc62'
+};
